3692bdc11e65a71a69ac8bd6a9a8169cf691a870
MaiaAndrey31 <maiaandrey31@outlook.com>
2025-07-12 19:17:06 -0300

    Architeture organize

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRoutes from './routes.jsx';
 import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <AppRoutes />
+        <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <AppRoutes />
+        </ThemeProvider>
+    
+    
   </React.StrictMode>
 );
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Home from './Pages/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./Pages/Trophy";
+import Home from "./Pages/Home";
 
 export default function AppRoutes() {
   return (
